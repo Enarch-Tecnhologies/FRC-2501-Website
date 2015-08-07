8723287a3377c1569f761504409ba1b04e4b53a1
Michael Wilke <mwilke16@moundsparkacademy.org>
2015-08-07 10:57:37 -0500

    Implementing Sponsor logos

--- Sponsors.php
+++ Sponsors.php
@@ -12,31 +12,20 @@
 	
 <div id="simages">
 	<div id="sponsor1">
-		<a href="http://www.jcpenney.com" id="sjcp"><img style="width:100%;" src="/images/jcpenney.png" alt="jcpenney Logo"></a>
+		<a href="#" id="sjcp"><img style="width:50%;" src="/resources/images/sponsors/3M.jpg" alt="3M Logo"></a>
+		<a href="#" id="snasa"><img style="width:50%;" src="/resources/images/sponsors/northlogo.png" alt="North Logo"></a>
+	</div>
 
-<a href="http://www.nasa.gov" id="snasa"><img style="width:100%;" src="/images/NASA_logo.png" alt="NASA Logo"></a>
-</div>
-
-<div id="sponsor2">
-<div id="saet">
-<a href="http://www.aetrium.com" style="padding:8px;display:block;"><img src="/images/Aetrium-Logo.png" alt="Aetrium Logo"></a><br />With Thanks to the Levesque Family<br />
-</div>
-<div id="ssmc">
-<a href="http://www.specialtymfg.com/" style="padding:10px;display:block;"><img src="/images/SMC-logo.gif" alt="Specialty Manufacturing Logo"></a><br />With Thanks to Daniel and Heidi McKeown, Jon Tomaszewski<br />
-</div>
+	<div id="sponsor2">
+		<div id="saet">
+			<a href="#" style="padding:8px;display:block;"><img src="/resources/images/sponsors/nettleton-infinite-photography.png" alt="nettleton-infinite-photography"></a>
+		</div>
+	</div>
 </div>
 
-<div id="sponsor3">
-<div id="sreell">
-<a href="http://www.reell.com" style="padding:10px;display:block;"><img src="/images/REELL-logo.jpg" alt="REELL Manufacturing Logo"></a>
-</div>
-<div id="smpapa">
-<a href="http://www.moundsparkacademy.org/currentfamilies/parentsassociation/" style="padding:10px;display:block;"><img src="/images/mpapa-logo.png" alt="Mounds Park Academy Parents Association logo"></a>
-</div></div>
-
-<div id="sponsor4">
-<div id="shome">
-<br /><br />
+	<div id="sponsor3">
+		<div id="shome">
+			<br /><br />
 <a style="font-size:25px;" href="http://www.homedepot.com">The Home Depot</a><br />
 </div></div>
 
