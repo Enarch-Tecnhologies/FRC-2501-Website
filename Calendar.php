3451c1315d943005f06d20a8fb2b249d7374fd6f
Michael Wilke <mwilke16@moundsparkacademy.org>
2015-08-06 21:13:52 -0500

    Clean out
    Cleaned out Files, and prepped for 2501 Redesign

--- Calendar.php
+++ Calendar.php
@@ -1,11 +1,14 @@
 <?php 
 $headerstuff = NULL;
-$pageTitle = "MPA Robotics - Contact";
+$pageTitle = "MPA Robotics - Blog";
 include('header.php');
 ?>
 
-<div id="content" style="text-align:center;">
+<div id="content" style="padding: 0px;">
+<iframe src="wordpress" style="width: 100%; border: 0px; height: 800px; margin-top: -5px;"></iframe>
 </div>
+
+
 <?php 
 $footerstuff = NULL;
 include('footer.php');
