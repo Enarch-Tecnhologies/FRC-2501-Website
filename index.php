acc16eedde1443a9cb447cf219a00c0b802a406f
Michael Wilke <mwilke16@moundsparkacademy.org>
2015-08-09 23:13:08 -0500

    Removed Sponsors from Main page

--- index.php
+++ index.php
@@ -11,13 +11,7 @@
 
 <!-- Begin Content Exp -->
 <div id="sponsors">
-	<h1>SPNSORS HERE</h1>
-
 	<div id="sponsorswoosh" class="swoosh">
-		<a href="http://www.nasa.gov/"><img src="/images/Sponsors/nasa.png" class="sponsors" id="nasa" alt="Nasa" /></a>
-		<a href="http://jcpenney.com"><img src="/images/Sponsors/jcpenny.svg" class="sponsors" id="jcpenny" alt="jcpenny" /></a>
-		<a href="http://moundsparkacademy.org/currentfamilies/parentsassociation/"><img src="/images/Sponsors/mpapa.png" class="sponsors" id="mpapa" alt="MPA Parents Association" /></a>
-		<a href="/Sponsors" id="moresponsors">More Sponsors -></a>
 	</div>
 </div>
 
